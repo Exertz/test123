7781f313f5d618cf4999119afcc9f402a32118ce
Cedrick Noel Dangcalan <dnacd123@gmail.com>
2023-07-25 16:03:48 +0800

    a

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta htpp-equive="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css" />
+    <title></title>
+</head>
+
+<body>
+    <header>
+        <nav style="background-color: #8AC4FF;">
+            <div class="navbar">
+                <div class="logo">
+                    <img src="./images/Logo.png" alt="" width="100px" />
+                </div>
+                <ul class="links">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="services.php">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <a href="#" class="action_btn">Book now</a>
+                <div class="toggle_btn"></div>
+            </div>
+        </nav>
+    </header>
+
+    <!-- container-1 -->
+    <div class="container-1 d-flex">
+        <div class="w-100 home-left d-flex flex-column align-items-center">
+            <p class="home-message" style="width: 50%; line-height: 3rem;">Transform Your Vision into Reality:
+                REGISTER Now!
+                to Book Unforgettable
+                Photo and Video Experiences</p>
+            <a href="registerpage.php" class="register_btn">Register</a>
+        </div>
+        <div class="w-100">
+            <h1>PUT CALENDER HERE</h1>
+        </div>
+
+
+        <!-- <a href="#" class="register_btn">Register</a>
+            <div class="toggle_btn:register"></div> -->
+
+        <!-- Wave -->
+        <!-- <div class="image-container">
+            <img src="./images/wave.png">
+        </div> -->
+    </div>
+
+    <!-- container-2 -->
+    <div class="container-2">
+        <h1 class="text-center" style="color:white;">Capture Every Moment, Just the Way You Dreamed</h1>
+        <h2 class="body-message">At Norlitz Bato Films, we understand the importance of life's special occasions. 
+            Our dedicated photo and video booking services are designed to capture the essence of your most treasured events. 
+            Whether it's a dream wedding, a memorable celebration, or an intimate gathering, we're here to turn your vision into stunning reality. 
+            Cherish your priceless moments with us.</h2>
+
+    <div class="container-2">
+    <div class="card">
+      <div class="img-box">
+        <img src="./images/Corporate.png">
+      </div>
+      <div class="content">
+        <h3>Corporate Events</h3>
+        <p>seminars,  product launches, team building activities, corporate parties </p>
+        <a href="#" class="btn">Book now</a>
+        <div class="icons">
+          <i class="fas fa-heart"></i>
+        </div>
+    </div>
+
+    <div class="card">
+      <div class="img-box">
+        <img src="./images/Weddings.png">
+      </div>
+      <div class="content">
+        <h3>Weddings</h3>
+        <p>engagement parties, ceremonies, bridal shower,  receptions</p>
+        <div class="icons"> 
+          <i class="fas fa-heart"></i>
+        </div>
+      <a href="#" class="btn">Book now</a>
+    </div>
+    </div>
+
+    <div class="card">
+      <div class="img-box">
+        <img src="./images/Social.png">
+      </div>
+      <div class="content">
+        <h3>Social Events</h3>
+        <p>birthday,  anniversaries, baby showers, graduations </p>
+        <div class="icons"> 
+          <i class="fas fa-heart"></i>
+        </div>
+      <a href="#" class="btn">Book now</a>
+    </div>
+
+    <div class="card">
+      <div class="img-box">
+        <img src="./images/Portrait.png">
+      </div>
+      <div class="content">
+        <h3>Portrait</h3>
+        <p>family portraits,  senior portraits, lifestyle photography</p>
+        <div class="icons"> 
+          <i class="fas fa-heart"></i>
+        </div>
+      <a href="#" class="btn">Book now</a>
+    </div>
+
+    <div class="card">
+      <div class="img-box">
+        <img src="./images/Commercial.png">
+      </div>
+      <div class="content">
+        <h3>Commercial Shoots</h3>
+        <p>advertising campaigns, fashion shoot</p>
+        <div class="icons"> 
+          <i class="fas fa-heart"></i>
+        </div>
+      <a href="#" class="btn">Book now</a>
+    </div>
+    </div>
+    </div>
+    <script src="https://kit.fontawesome.com/your-font-awesome-kit.js" crossorigin="anonymous"></script>
+    
+    <div class="container-3">
+    <!-- <img class="image-wave" src="./images/wave.png" alt="" height="220px" width="100%"> -->
+</body>
+
+</html>
