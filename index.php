b8460b1a43f3a73bb750d2d3b31b93ecb7fe85e7
Cedrick Noel Dangcalan <dnacd123@gmail.com>
2023-07-27 12:16:48 +0800

    a

--- index.php
+++ index.php
@@ -1,33 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" dir="ltr">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta htpp-equive="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <link rel="stylesheet" href="style.css" />
-    <title></title>
+<meta charset="utf-8">
+    <title>NorlitzBatoFilms | Cedrick Gwapo</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
 </head>
 
 <body>
-    <header>
-        <nav style="background-color: #8AC4FF;">
-            <div class="navbar">
-                <div class="logo">
-                    <img src="./images/Logo.png" alt="" width="100px" />
-                </div>
-                <ul class="links">
-                    <li><a href="index.php">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="services.php">Services</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-                <a href="#" class="action_btn">Book now</a>
-                <div class="toggle_btn"></div>
-            </div>
+<header>
+        <nav>
+            <input type="checkbox" id="check">
+            <label for="check" class="checkbtn">
+                <i class="fas fa-bars"></i>
+            </label>
+                <img src="./images/Logo.png" width="100px">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a class="active" href="#">Book now</a></li>
+            </ul>
         </nav>
     </header>
 
@@ -56,82 +52,27 @@
 
     <!-- container-2 -->
     <div class="container-2">
-        <h1 class="text-center" style="color:white;">Capture Every Moment, Just the Way You Dreamed</h1>
-        <h2 class="body-message">At Norlitz Bato Films, we understand the importance of life's special occasions. 
+            <h1 class="text-center" style="color:white;">Capture Every Moment, Just the Way You Dreamed</h1>
+            <h4 class="body-message">At Norlitz Bato Films, we understand the importance of life's special occasions. 
             Our dedicated photo and video booking services are designed to capture the essence of your most treasured events. 
             Whether it's a dream wedding, a memorable celebration, or an intimate gathering, we're here to turn your vision into stunning reality. 
-            Cherish your priceless moments with us.</h2>
+            Cherish your priceless moments with us.</h4>
 
-    <div class="container-2">
-    <div class="card">
-      <div class="img-box">
-        <img src="./images/Corporate.png">
-      </div>
-      <div class="content">
-        <h3>Corporate Events</h3>
-        <p>seminars,  product launches, team building activities, corporate parties </p>
-        <a href="#" class="btn">Book now</a>
-        <div class="icons">
-          <i class="fas fa-heart"></i>
-        </div>
-    </div>
+         <div class="cards">
+            <div class="card">
+                <img src="./images/Corporate.png">
+                <div class="card__content">
+                    <h3>Corporate Events</h3>
+                    <p>seminars,  product launches, team building activities, corporate parties</P>
+            </div>
 
-    <div class="card">
-      <div class="img-box">
-        <img src="./images/Weddings.png">
-      </div>
-      <div class="content">
-        <h3>Weddings</h3>
-        <p>engagement parties, ceremonies, bridal shower,  receptions</p>
-        <div class="icons"> 
-          <i class="fas fa-heart"></i>
-        </div>
-      <a href="#" class="btn">Book now</a>
+            <div class="card">
+                <img src="./images/Corporate.png">
+                <div class="card__content">
+                    <h3>Corporate Events</h3>
+                    <p>seminars,  product launches, team building activities, corporate parties</P>
+            </div>
     </div>
-    </div>
-
-    <div class="card">
-      <div class="img-box">
-        <img src="./images/Social.png">
-      </div>
-      <div class="content">
-        <h3>Social Events</h3>
-        <p>birthday,  anniversaries, baby showers, graduations </p>
-        <div class="icons"> 
-          <i class="fas fa-heart"></i>
-        </div>
-      <a href="#" class="btn">Book now</a>
-    </div>
-
-    <div class="card">
-      <div class="img-box">
-        <img src="./images/Portrait.png">
-      </div>
-      <div class="content">
-        <h3>Portrait</h3>
-        <p>family portraits,  senior portraits, lifestyle photography</p>
-        <div class="icons"> 
-          <i class="fas fa-heart"></i>
-        </div>
-      <a href="#" class="btn">Book now</a>
-    </div>
-
-    <div class="card">
-      <div class="img-box">
-        <img src="./images/Commercial.png">
-      </div>
-      <div class="content">
-        <h3>Commercial Shoots</h3>
-        <p>advertising campaigns, fashion shoot</p>
-        <div class="icons"> 
-          <i class="fas fa-heart"></i>
-        </div>
-      <a href="#" class="btn">Book now</a>
-    </div>
-    </div>
-    </div>
-    <script src="https://kit.fontawesome.com/your-font-awesome-kit.js" crossorigin="anonymous"></script>
-    
     <div class="container-3">
     <!-- <img class="image-wave" src="./images/wave.png" alt="" height="220px" width="100%"> -->
 </body>
